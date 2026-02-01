7730b8e19f131bb1329ecc7625dcd55da0399486
iuculano <82924525+iuculano@users.noreply.github.com>
2026-02-01 03:27:20 +0000

    Revisit this...

--- apps/gateway-api/src/api/logs/logs.handlers.ts
+++ apps/gateway-api/src/api/logs/logs.handlers.ts
@@ -20,7 +20,6 @@ app.openapi(Routes.getLogData, async (c) => {
   return c.json(result, 200);
 });
 
-
 app.openapi(Routes.listLogs, async (c) => {
   const query = c.req.valid('query');
   const result = await Service.listLogs(query);
@@ -28,19 +27,21 @@ app.openapi(Routes.listLogs, async (c) => {
   return c.json(result, 200);
 });
 
-app.openapi(Routes.createLog, async (c) => {
-  const json = c.req.valid('json');
-  const result = await Service.createLog(json);
-
-  return c.json(result, 201);
-});
-
-app.openapi(Routes.updateLog, async (c) => {
-  const params = c.req.valid('param');
-  const json = c.req.valid('json');
-  const result = await Service.updateLog(params.id, json);  
-
-  return c.json(result, 200);
-});
+// Privleged
+//
+// app.openapi(Routes.createLog, async (c) => {
+//   const json = c.req.valid('json');
+//   const result = await Service.createLog(json);
+// 
+//   return c.json(result, 201);
+// });
+// 
+// app.openapi(Routes.updateLog, async (c) => {
+//   const params = c.req.valid('param');
+//   const json = c.req.valid('json');
+//   const result = await Service.updateLog(params.id, json);  
+// 
+//   return c.json(result, 200);
+// });
 
 export default app;
